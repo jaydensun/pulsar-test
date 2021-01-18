1d17dfcf52bf6dd50db8bee1bcf03ee745be02e0
tandysun <tandysun@tencent.com>
2021-01-18 23:13:04 +0800

    delete unused

--- pulsar-test/src/main/java/com/jayden/Config.java
+++
@@ -1,6 +0,0 @@
-package com.jayden;
-
-public class Config {
-    public static final String SERVER = "pulsar://106.52.255.68:6650,159.75.138.157:6650,106.52.154.114:6650";
-    public static final String TOPIC_NAME = "test-topic-888";
-}
